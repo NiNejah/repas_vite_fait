c85e59190f1899d59a15f2a356c26b7db0d77cd0
Théo Brégégère <bregegere.theo@outlook.fr>
2023-11-26 23:48:10 +0100

    ajout composant LogoButton

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+import LogoButton from './components/LogoButton.vue'
 </script>
 
 <template>
@@ -14,6 +15,7 @@ import HelloWorld from './components/HelloWorld.vue'
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
+      <LogoButton routerlink="/about" image="logo.svg"></LogoButton>
     </div>
   </header>
 
