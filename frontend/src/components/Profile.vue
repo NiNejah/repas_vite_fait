8db99d66812abc5c905f777245151095bd36015d
NiNejah <almawlanejeh@gmail.com>
2024-01-16 20:29:54 +0100

    fix: profile page

--- frontend/src/components/Profile.vue
+++ frontend/src/components/Profile.vue
@@ -1,28 +1,41 @@
 <script setup>
 import { ref } from 'vue';
+import { useUserStore } from "../stores/userStore";
+const store = useUserStore();
 
+console.log(store.user.data)
 const user = ref({
-    name: 'alahmat',
-    email: 'ali@example.com',
-    // Ajoutez d'autres propriétés du profil
+    name: store.user.data.username,
+    email: store.user.data.email,
+    vegetarian: store.user.data.vegetarian,
 });
-</script>
 
+const updateUser = () => {
+    // Mettez à jour les valeurs de l'utilisateur ici
+    // Vous pouvez également effectuer des validations si nécessaire
+};
+
+</script>
 
 <template>
     <div class="container mx-auto p-4">
-        <div class="max-w-md mx-auto bg-white rounded-md overflow-hidden shadow-md">
+        <div>
+            <h1> Your profile</h1>
+        </div>
+        <div class="profil max-w-md mx-auto bg-white rounded-md overflow-hidden shadow-md">
             <div class="p-4">
-                <h2 class="mt-2 text-xl font-semibold text-gray-800"><font-awesome-icon icon="user"/> {{ user.name }}</h2>
-                <p class="text-gray-600">{{ user.email }}</p>
+                <h2 class="mt-2 text-xl font-semibold text-gray-800">
+                    <font-awesome-icon icon="user"/> {{ user.name }}
+                </h2>
+                <p class="text-gray-600">Email: {{ user.email }}</p>
+                <p class="text-gray-600" v-if="user.vegetarian">Vegetarian </p>
             </div>
         </div>
     </div>
 </template>
-  
 
-  
 <style scoped>
-
+.profil {
+    margin-top: 6em;
+}
 </style>
-  
