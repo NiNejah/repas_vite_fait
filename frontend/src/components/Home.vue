b9fdbe11ec318545241e43ef5cf15d99dc123111
NiNejah <almawlanejeh@gmail.com>
2024-01-16 20:08:48 +0100

    fix : home bugs

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -1,5 +1,5 @@
 <script setup >
-import {ref, computed, toRefs} from 'vue';
+import { ref, computed, toRefs } from 'vue';
 import recipesList from '../data/recipes.js';
 import Recipe from './Recipe.vue';
 import { useUserStore } from '../stores/userStore.js';
@@ -11,14 +11,14 @@ const searchQuery = ref('');
 const recipes = ref([]);
 const keywords = ref([]);
 
-const signout = () =>{
+const signout = () => {
   console.log("disconnected");
   userStore.disconnect();
   userStore.setUser(null);
 }
 
 const addKeyword = () => {
-  if(searchQuery.value !== ''){
+  if (searchQuery.value !== '') {
     keywords.value.push(searchQuery.value);
     let newArray = keywords._rawValue;
     keywords.value = newArray;
@@ -33,22 +33,22 @@ const deleteKeyword = (keyword) => {
 
 const searchRecipes = async () => {
   try {
-    if(keywords._rawValue.length > 0){
+    if (keywords._rawValue.length > 0) {
       const filters = keywords._rawValue.join(' ');
       let health = "";
-      if(document.getElementById("vegetarian").checked) health += "&health=vegetarian";
-      if(document.getElementById("peanut").checked) health += "&health=peanut-free";
-      if(document.getElementById("pork").checked) health += "&health=pork-free";
+      if (document.getElementById("vegetarian").checked) health += "&health=vegetarian";
+      if (document.getElementById("peanut").checked) health += "&health=peanut-free";
+      if (document.getElementById("pork").checked) health += "&health=pork-free";
       console.log(filters);
-      const response = await fetch("https://api.edamam.com/api/recipes/v2?type=public&q=" + filters 
-      + "&app_id=01c306cf&app_key=6179f34f1acea7368bcd5d4020b90b0c" + health);
-      if(response.status === 200){
+      const response = await fetch("https://api.edamam.com/api/recipes/v2?type=public&q=" + filters
+        + "&app_id=01c306cf&app_key=6179f34f1acea7368bcd5d4020b90b0c" + health);
+      if (response.status === 200) {
         //removeRecipesCards();
 
         const json = await response.json();
         const jsonRecipes = json.hits;
         let listRecipes = [];
-        for(const hit of jsonRecipes){
+        for (const hit of jsonRecipes) {
           const recipe = hit.recipe;
           listRecipes.push(recipe);
         }
@@ -63,7 +63,7 @@ const searchRecipes = async () => {
 
 const removeRecipesCards = () => {
   let cards = document.getElementsByClassName("recipe");
-  while(cards.length > 0){
+  while (cards.length > 0) {
     cards[0].parentNode.removeChild(cards[0]);
   }
 }
@@ -78,21 +78,21 @@ const search = () => {
   <div id="app" class="bg-gray-100">
     <nav>
       <ul v-if="!isConnected">
-        <li >
-          <router-link to="/registre" ><font-awesome-icon icon="user-plus" /> Sign Up</router-link>
+        <li>
+          <router-link to="/registre"><font-awesome-icon icon="user-plus" /> Sign Up</router-link>
         </li>
         <li>
-          <router-link to="/login" ><font-awesome-icon icon="sign-in-alt" /> Login</router-link>
+          <router-link to="/login"><font-awesome-icon icon="sign-in-alt" /> Login</router-link>
         </li>
       </ul>
       <ul v-else>
-        <li >
+        <li>
           <!-- <b-button class="button" variant="outline-secondary" @click="signout" >Sign Out</b-button> -->
           <router-link to="" @click="signout"><font-awesome-icon icon="sign-out-alt" /> Sign Out</router-link>
         </li>
       </ul>
     </nav>
-    <div class="px-4 py-5 text-center bgr-home food "> 
+    <div class="px-4 py-5 text-center bgr-home food ">
       <h1 class="display-5 fw-bold home-title col-8 mx-auto"> Repas Vite Fait </h1>
       <div class="col-lg-6 mx auto">
         <p class="lead mb-4"></p>
@@ -100,47 +100,44 @@ const search = () => {
     </div>
 
     <div class="flex items-center justify-center space-x-2 search">
-      <input
-        v-model="searchQuery"
-        type="text"
+      <input v-model="searchQuery" type="text"
         class="border p-2 rounded focus:outline-none focus:ring focus:border-blue-300 w-full md:w-3/4 lg:w-1/2"
-        placeholder="Search recipe..."
-      />
-      <button
-        @click="search"
-        class="bg-gray-800 text-white px-4 py-2 rounded hover:bg-gray-600 focus:outline-none focus:ring focus:border-blue-300"
-      >
-      Search
+        placeholder="Search recipe..." />
+      <button @click="search"
+        class="bg-gray-800 text-white px-4 py-2 rounded hover:bg-gray-600 focus:outline-none focus:ring focus:border-blue-300">
+        Search
       </button>
-    <div id="search">
-      <input v-model="searchQuery" class="searchBar" type="search" placeholder="Add an ingredient to your list">
-      <b-button variant="warning" @click="addKeyword">+</b-button>
-      <b-button variant="success" @click="searchRecipes">Search</b-button>
-    </div>
-    <div id="checkboxes">
-      <div class="checkbox">
-        <input type="checkbox" id="vegetarian" name="vegetarian"/>
-        <label for="vegetarian">Vegetarian</label>
+      <div id="search">
+        <input v-model="searchQuery" class="searchBar" type="search" placeholder="Add an ingredient to your list">
+        <b-button variant="warning" @click="addKeyword">+</b-button>
+        <b-button variant="success" @click="searchRecipes">Search</b-button>
       </div>
-      <div class="checkbox">
-        <input type="checkbox" id="peanut" name="peanut"/>
-        <label for="peanut">Peanut-free</label>
+      <div id="checkboxes">
+        <div class="checkbox">
+          <input type="checkbox" id="vegetarian" name="vegetarian" />
+          <label for="vegetarian">Vegetarian</label>
+        </div>
+        <div class="checkbox">
+          <input type="checkbox" id="peanut" name="peanut" />
+          <label for="peanut">Peanut-free</label>
+        </div>
+        <div class="checkbox">
+          <input type="checkbox" id="pork" name="pork" />
+          <label for="pork">Pork-free</label>
+        </div>
       </div>
-      <div class="checkbox">
-        <input type="checkbox" id="pork" name="pork"/>
-        <label for="pork">Pork-free</label>
+      <div id="keywords">
+        <div class="keyword" v-for="keyword of keywords" :key="keyword">
+          <p>{{ keyword }}</p>
+          <button class="delKeyword" @click="deleteKeyword(keyword)"><font-awesome-icon icon="x" /></button>
+        </div>
       </div>
-    </div>
-    <div id="keywords">
-      <div class="keyword" v-for="keyword of keywords" :key="keyword">
-        <p>{{ keyword }}</p>
-        <button class="delKeyword" @click="deleteKeyword(keyword)"><font-awesome-icon icon="x"/></button>
-      </div>
-    </div>
-    <div id="recipes">
-      <div class="col-md-4">
-        <div class="recipe" v-for="recipe in recipes" :key="recipe.id">
-          <Recipe :id="recipe.uri" :name="recipe.label" :url="recipe.url" :image_url="recipe.image" :calories="recipe.calories" :servings="recipe.yield" :source="recipe.source"/>
+      <div id="recipes">
+        <div class="col-md-4">
+          <div class="recipe" v-for="recipe in recipes" :key="recipe.id">
+            <Recipe :id="recipe.uri" :name="recipe.label" :url="recipe.url" :image_url="recipe.image"
+              :calories="recipe.calories" :servings="recipe.yield" :source="recipe.source" />
+          </div>
         </div>
       </div>
     </div>
@@ -148,12 +145,12 @@ const search = () => {
 </template>
 
 <style scoped>
-
-.search{
+.search {
   margin-top: 10rem;
-  
+
 }
-.bgr-home{
+
+.bgr-home {
   /* background-image: url('https://womensfitness.co.uk/wp-content/uploads/sites/3/2022/11/Shutterstock_1675475479.jpg?w=900'); */
   /* background-image: url('https://upload.wikimedia.org/wikipedia/commons/thumb/6/6d/Good_Food_Display_-_NCI_Visuals_Online.jpg/800px-Good_Food_Display_-_NCI_Visuals_Online.jpg?20100603030321'); */
   background-image: url('https://cdn.britannica.com/36/123536-050-95CB0C6E/Variety-fruits-vegetables.jpg');
@@ -163,13 +160,14 @@ const search = () => {
 }
 
 .bgr-home h1 {
-  color: white; 
+  color: white;
 }
 
 #app {
   text-align: center;
   font-family: Arial, Helvetica, sans-serif;
 }
+
 ul {
   list-style-type: none;
   margin: 0;
@@ -178,9 +176,11 @@ ul {
   /* background-color: #333; */
   background-color: #1d2834;
 }
+
 li {
   float: right;
 }
+
 li a {
   display: block;
   color: white;
@@ -188,6 +188,7 @@ li a {
   padding: 14px 16px;
   text-decoration: none;
 }
+
 .error {
   color: red;
   font-weight: bold;
