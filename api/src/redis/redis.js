5cf41aa9ab8eb4bad6bd8ca26bc1338b6be709fe
NiNejah <almawlanejeh@gmail.com>
2024-01-18 00:44:19 +0100

    fix: redis

--- api/src/redis/redis.js
+++ api/src/redis/redis.js
@@ -5,26 +5,4 @@ const redis = new Redis({
     port: process.env.REDIS_PORT,
 });
 
-
-
-// Middleware for caching
-export const cacheMiddleware = async (req, res, next) => {
-    const key = req.originalUrl || req.url;
-
-    try {
-        const data = await redis.get(key);
-
-        if (data !== null) {
-            console.log(data);
-            res.status(200).json(JSON.parse(data));
-        } else {
-            console.log("rien");
-            next();
-        }
-    } catch (error) {
-        console.error('Error in cacheMiddleware:', error);
-        next();
-    }
-};
-
 export default redis ; 
