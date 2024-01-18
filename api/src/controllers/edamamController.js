c5995620de0d52a5f88d454a2eb74fac7b1e02f9
NiNejah <almawlanejeh@gmail.com>
2024-01-18 01:16:16 +0100

    fix: edamam

--- api/src/controllers/edamamController.js
+++ api/src/controllers/edamamController.js
@@ -4,18 +4,18 @@ import redis from '../redis/redis.js';
 
 config();
 
-const edamamID = process.env.EDAMAM_ID || "01c306cf";
-const edamamKey =  process.env.EDAMAM_KEY || "6179f34f1acea7368bcd5d4020b90b0c";
+const edamamID = process.env.EDAMAM_ID || "01c306cf";
+const edamamKey = process.env.EDAMAM_KEY || "6179f34f1acea7368bcd5d4020b90b0c";
 
 export const getRecipes = async (req, res) => {
-    const cacheKey = req.body.filters + req.body.health;
     try {
-        // const cachedData = await getAsync(cacheKey);
-        const cachedData = apiCache.get(cacheKey);
+        const key = `recipes${req.body.filters}${req.body.health}`;
+        const cachedData = await redis.get(key);
 
-        if (cachedData) {
-            console.log("cache");
-            res.json(cachedData);
+        if (cachedData !== null) {
+            console.log('Cache hit:', cachedData);
+            const parsedCachedData = JSON.parse(cachedData);
+            res.status(parsedCachedData.success ? 200 : 404).json(parsedCachedData);
         } else {
             console.log("api");
             const apiUrl = `https://api.edamam.com/api/recipes/v2?type=public&q=${req.body.filters}&app_id=${edamamID}&app_key=${edamamKey}${req.body.health}`;
@@ -25,13 +25,13 @@ export const getRecipes = async (req, res) => {
 
             console.log("API Response:", responseData);
             console.log(apiUrl);
+            
+            console.log('Cache miss. Fetching data from the database:', responseData);
 
-            if (response.ok) {
-                apiCache.set(cacheKey,responseData, 60*5);
-                res.status(200).json(responseData);
-            } else {
-                res.status(404).json({ success: false, message: 'Recipes not found' });
-            }
+            res.status(response.ok ? 200 : 404).json({ success: response.ok ,data : responseData});
+
+            // Update the cache with the fetched data
+            await redis.set(key, JSON.stringify({ success: response.ok ,data : responseData}));
         }
     } catch (error) {
         console.error("Error:", error);
@@ -39,34 +39,36 @@ export const getRecipes = async (req, res) => {
     }
 };
 
-
 export const getRecipeByID = async (req, res) => {
-    const cacheKey = req.params.id;
     try {
-        const cachedData = apiCache.get(cacheKey);
+        const key = `recipesID${req.params.id}`;
+        const cachedData = await redis.get(key);
 
-        if (cachedData) {
-            console.log("cache");
-            res.json(cachedData);
+        if (cachedData !== null) {
+            console.log('Cache hit:', cachedData);
+            const parsedCachedData = JSON.parse(cachedData);
+            res.status(parsedCachedData.success ? 200 : 404).json(parsedCachedData);
         } else {
             console.log("api");
-            const apiUrl = `https://api.edamam.com/api/recipes/v2/${cacheKey}?type=public&app_id=${edamamID}&app_key=${edamamKey}`;
+            const apiUrl = `https://api.edamam.com/api/recipes/v2/${req.params.id}?type=public&app_id=${edamamID}&app_key=${edamamKey}`;
+
             const response = await fetch(apiUrl);
             const responseData = await response.json();
 
             console.log("API Response:", responseData);
             console.log(apiUrl);
+            
+            console.log('Cache miss. Fetching data from the database:', responseData);
 
-            if (response.ok) {
-                apiCache.set(cacheKey,responseData, 60*5);
-                res.status(200).json(responseData);
-            } else {
-                res.status(404).json({ success: false, message: 'Recipe not found' });
-            }
+            res.status(response.ok ? 200 : 404).json({ success: response.ok ,data : responseData});
+
+            // Update the cache with the fetched data
+            await redis.set(key, JSON.stringify({ success: response.ok ,data : responseData}));
         }
     } catch (error) {
         console.error("Error:", error);
         res.status(500).json({ success: false, message: 'Internal Server Error' });
     }
 };
+
 
